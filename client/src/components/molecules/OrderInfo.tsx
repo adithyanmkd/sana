9a90064e322280af9519e2435d6827d68853b771
adithyan <adithyanmkd123@gmail.com>
2024-11-04 10:56:56 +0530

    navbar modified

--- client/src/components/molecules/OrderInfo.tsx
+++ client/src/components/molecules/OrderInfo.tsx
@@ -11,23 +11,27 @@ const arrowRight = {
 
 const OrderInfo = () => {
   return (
-    <div className="bg-secondaryBg">
+    <div className="bg-secondaryBg md:py-8">
       <div className="relative -top-[1px] mx-auto grid w-full max-w-screen-lg items-center space-y-4 px-5 py-6 md:grid-cols-3">
         <div className="flex space-x-7">
           <img src={DeliveryTruck} alt="delivery truck" />
-          <p className="text-lg font-semibold text-[#EBEBEB]">
+          <p className="text-lg font-semibold text-[#EBEBEB] md:text-2xl">
             06 740<span className="block">8655</span>
           </p>
         </div>
         <div className="flex space-x-7">
           <img src={MsgIcon} alt="msg icon" />
-          <p className="text-lg font-semibold text-[#EBEBEB]">
+          <p className="text-lg font-semibold text-[#EBEBEB] md:text-2xl">
             +971 56 75 33072
             <span className="block">info@sanawater.com </span>
           </p>
         </div>
         <div className="flex justify-end">
-          <Button label="Order Now" icon={arrowRight} />
+          <Button
+            classname="md:h-[50px] md:px-6"
+            label="Order Now"
+            icon={arrowRight}
+          />
         </div>
       </div>
     </div>
--- client/src/components/organisms/Navbar.tsx
+++ client/src/components/organisms/Navbar.tsx
@@ -3,6 +3,30 @@ import MenuIcon from "../../assets/icons/menu-icon.svg";
 import MobileSidebar from "./MobileSidebar";
 
 import { useState } from "react";
+import Button from "../atoms/Button";
+
+const navLinks = [
+  {
+    name: "home",
+    url: "#",
+  },
+  {
+    name: "about us",
+    url: "#",
+  },
+  {
+    name: "services",
+    url: "#",
+  },
+  {
+    name: "products",
+    url: "#",
+  },
+  {
+    name: "career",
+    url: "#",
+  },
+];
 
 const Navbar = () => {
   const [sidebarIsActive, setSidebarIsActive] = useState(false);
@@ -15,16 +39,29 @@ const Navbar = () => {
         <MobileSidebar toggleMenu={toggleMenu} />
       ) : (
         <>
-          <div className="absolute z-10 mt-1 flex w-full items-center justify-between px-5">
+          <div className="absolute left-1/2 z-10 mt-3 flex w-full max-w-screen-lg -translate-x-1/2 transform items-center justify-between px-5">
             <div>
-              <img
-                className="w-16 sm:w-24 md:w-36"
-                src={Logo}
-                alt="sana logo"
-              />
+              <img className="w-16 sm:w-24" src={Logo} alt="sana logo" />
             </div>
-            <div className="cursor-pointer" onClick={toggleMenu}>
-              <img className="md:hidden" src={MenuIcon} alt="menu icon" />
+            <div className="cursor-pointer">
+              <img
+                className="md:hidden"
+                onClick={toggleMenu}
+                src={MenuIcon}
+                alt="menu icon"
+              />
+              <div className="hidden space-x-5 rounded-[30px] bg-white bg-opacity-80 px-2 py-2 md:flex">
+                {navLinks.map((data, index) => (
+                  <a
+                    className="text-nowrap rounded-[24px] px-2 py-2 capitalize text-[#505050] opacity-100 transition-all hover:scale-105 hover:bg-white hover:text-black lg:px-4"
+                    key={index}
+                    href={data.url}
+                  >
+                    {data.name}
+                  </a>
+                ))}
+                <Button label="Contact Us" classname="text-nowrap" />
+              </div>
             </div>
           </div>
         </>
