2b607ef22cc752bf2952af2ecf6c4ff23a00b568
adithyan <adithyanmkd123@gmail.com>
2024-12-03 13:57:12 +0530

    why choose section creating...

--- client/src/components/molecules/WhyChooseCard.tsx
+++ client/src/components/molecules/WhyChooseCard.tsx
@@ -1,5 +1,28 @@
-const WhyChooseCard = () => {
-  return <div></div>;
+type CardTypes = {
+  icon: {
+    path: string;
+    alt: string;
+  };
+  label: string;
+  description: string;
+  classname?: string;
+};
+
+const WhyChooseCard = ({
+  icon,
+  label,
+  description,
+  classname = "",
+}: CardTypes) => {
+  return (
+    <div
+      className={`max-w-[500px] rounded-md border-b bg-white px-2 py-3 shadow-md ${classname}`}
+    >
+      <img src={icon.path} alt={icon.alt} />
+      <h4 className="mt-5 text-lg font-bold text-[#12141D]">{label}</h4>
+      <p className="mt-2.5 text-[#494949]">{description}</p>
+    </div>
+  );
 };
 
 export default WhyChooseCard;
--- client/src/components/organisms/WhyChoose.tsx
+++ client/src/components/organisms/WhyChoose.tsx
@@ -1,4 +1,40 @@
+import icon1 from "../../assets/icons/whyChoose/icon1.svg";
+import icon2 from "../../assets/icons/whyChoose/icon2.svg";
+import icon3 from "../../assets/icons/whyChoose/icon3.svg";
+
 import WhyChooseCard from "../molecules/WhyChooseCard";
+
+const whyChooseCardContent = [
+  {
+    icon: {
+      path: icon1,
+      alt: "icon 1",
+    },
+    label: "Fast and Reliable Service",
+    description: "Our efficient delivery ensures you never run out of water.",
+    classname: "border-[#4147D5] shadow-[#7f80cdab]",
+  },
+  {
+    icon: {
+      path: icon2,
+      alt: "icon 2",
+    },
+    label: "Customer Loyalty",
+    description:
+      "Many of our customers have trusted us for over 15 to 20 years—a testament to the quality of our products and services.",
+    classname: "border-[#008EFF] shadow-[#7bc0f8b5]",
+  },
+  {
+    icon: {
+      path: icon3,
+      alt: "icon 3",
+    },
+    label: "Leading in the UAE",
+    description:
+      "We are proud to be among the top water providers in the region, recognized for our consistent excellence and customer satisfaction.",
+    classname: "border-[#14B082] shadow-[#6ec9ae74]",
+  },
+];
 
 const WhyChoose = () => {
   return (
@@ -13,8 +49,10 @@ const WhyChoose = () => {
         purity, safety, and trust. Join thousands of satisfied customers who
         rely on us daily for their hydration needs.
       </p>
-      <div>
-        <WhyChooseCard />
+      <div className="mx-auto mt-6 grid space-y-8 md:max-w-[740px] md:grid-cols-2 md:gap-8 md:space-y-0 lg:max-w-[1200px] lg:grid-cols-3">
+        {whyChooseCardContent.map((data, index) => (
+          <WhyChooseCard key={index} {...data} />
+        ))}
       </div>
     </div>
   );
